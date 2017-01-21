ffdaf466a2dfdcd253eb641d55dd622e3be1ab54
Matin Hormati <mthormati@gmail.com>
2017-01-21 15:26:48 -0500

    minor changes

--- Apollo11/app.js
+++ Apollo11/app.js
@@ -22,11 +22,11 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(__dirname + 'public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + 'public'));
 
 app.use(function(req,res,next){
 	req.db = db;
