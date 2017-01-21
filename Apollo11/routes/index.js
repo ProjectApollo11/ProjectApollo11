c14fd7b31e5bf43b5481434188d719b273c44af3
Aung Myat <amyat@purdue.edu>
2017-01-21 00:08:39 -0500

    changed index.js in routes

--- Apollo11/routes/index.js
+++ Apollo11/routes/index.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var io = require('socket.io')();
+
+
+
+var numuser = 0;
+
+router.get('/',function(req,res,next){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  numuser++;
+  console.log('user' + numuser +' connected');
+  socket.on('disconnect', function(){
+  	console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
+  	console.log('message: ' + msg);
+  });
+});
+
+io.on('connection',function(socket){
+	socket.on('user image',function(image){
+		io.emit('addimage','Image Received: ',image);
+	});
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
