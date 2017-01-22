80dec6323eefa02708686a2b7711f0edada3646a
sbenchik <stevebenchik@gmail.com>
2017-01-21 19:45:48 -0500

    Adds some firebase authentication stuff:

--- Apollo11/app.js
+++ Apollo11/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
+var firebase = require('firebase');
 
 //database stuff
 var mongo = require('mongodb');
